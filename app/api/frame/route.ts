940a1ed053e4a1e8fa2a0543155aa27080847cf6
lim0n3 <simonestaffa96@gmail.com>
2024-01-28 22:47:44 +0100

    add console log

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
+  console.log('API frame route called');
   return new NextResponse(`<!DOCTYPE html><html><head>
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="https://lemon-frame.vercel.app/img-2.png" />
