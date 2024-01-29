48dfb55ca3d80d11dde8f9ed33bca8f30d1faa3a
lim0n3 <simonestaffa96@gmail.com>
2024-01-30 00:59:54 +0100

    change ipfs url

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -15,8 +15,8 @@ export const NFT_DESCRIPTION =
 export const NFT_IMAGE_URL = 'https://i.imgur.com/uzSo4ew.png';
 
 export const INITIAL_IMAGE_URL =
-  ' ipfs://Qme9BVhNPfsaKKhPy7k1QWoMnmqrFZMjibXEKjmNEa8ADH/yahoozora-cropped.gif';
+  'https://gateway.ipfs.io/ipfs/Qme9BVhNPfsaKKhPy7k1QWoMnmqrFZMjibXEKjmNEa8ADH/yahoozora-cropped.gif';
 export const SUCCESS_CLAIM_IMAGE_URL =
-  'ipfs://Qme9BVhNPfsaKKhPy7k1QWoMnmqrFZMjibXEKjmNEa8ADH/Slice%202.png';
+  'https://gateway.ipfs.io/ipfs/Qme9BVhNPfsaKKhPy7k1QWoMnmqrFZMjibXEKjmNEa8ADH/Slice%202.png';
 export const ALREADY_CLAIMED_IMAGE_URL = 'https://i.imgur.com/uzSo4ew.png';
 export const NOT_RECASTED_IMAGE_URL = 'https://i.imgur.com/uzSo4ew.png';
